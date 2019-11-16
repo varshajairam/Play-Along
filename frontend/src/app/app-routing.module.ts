7255a8557b4f9c7db61b240fc155374607cb510f
Ashwini Talele <ashtalele70@gmail.com>
2019-11-16 15:37:30 -0800

    Login page frontend

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
   {
     path: 'list',
     loadChildren: () => import('./list/list.module').then(m => m.ListPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: './auth/login/login.module#LoginPageModule'
   }
 ];
 
