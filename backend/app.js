fa2ae80b81f1f52334cebe8f65d141bc657cedcc
Ronak Shah <ronakshah2895@gmail.com>
2019-11-16 18:46:16 -0800

    added login and logout functionality with dummy authentication

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,32 @@
 require('dotenv').config();
 const express = require('express');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
 const app = express();
+var login_register_handler = require('./login_register_handler');
 const port = 3000;
 
-var login_register_handler = require('./login_register_handler');
+app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json({ type: 'application/*+json' }));
+app.use(session({
+	secret: 'keyboard cat',
+  	resave: false,
+  	saveUninitialized: true,
+	cookie: { secure: false }
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(login_register_handler.loginHandler));
+passport.serializeUser(login_register_handler.serializeUser);
+passport.deserializeUser(login_register_handler.deserializeUser);
 
-app.post('/login', login_register_handler.loginHandler);
+app.post('/login', passport.authenticate('local'), function(req, res) {
+	res.send("Success");
+});
+app.get('/logout', login_register_handler.logoutHandler);
 app.post('/register', login_register_handler.registerHandler);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- backend/login_register_handler.js
+++ backend/login_register_handler.js
@@ -1,5 +1,20 @@
-function loginHandler(req, res) {
-	res.send("Hello World");
+function loginHandler(username, password, done) {
+	if(username == "test" && password == "test")
+		return done(null, {username: "test"})
+	return done(null, false, { message: 'Incorrect username or password.' });
+}
+
+function serializeUser(user, done) {
+	done(null, 1)
+}
+
+function deserializeUser(id, done) {
+	done(null, {username: "test"})
+}
+
+function logoutHandler(req, res) {
+	req.logout();
+	res.send("Success");
 }
 
 function registerHandler(req, res) {
@@ -8,5 +23,8 @@ function registerHandler(req, res) {
 
 module.exports = {
 	loginHandler: loginHandler,
-	registerHandler: registerHandler
+	logoutHandler: logoutHandler,
+	registerHandler: registerHandler,
+	serializeUser: serializeUser,
+	deserializeUser: deserializeUser
 }
