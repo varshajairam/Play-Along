e42af9454c5b51e6c3ec425480f448a079dd0ac3
terrybelinda <terrylinda13@gmail.com>
2019-11-18 15:13:55 -0800

    code clean up

--- backend/db_connectio.js
+++ backend/db_connectio.js
@@ -1,66 +1,29 @@
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "terry"
+    host: "localhost",
+    user: "root",
+    password: "terry"
 });
 
-var sql = "insert into playalong.user values('1','Belinda','belinda,terry@gmail.com','oo',1,'2016-01-01',9888,'pp','pp','io','IN',90)";
-var selectSql="Select * from users" 
 
-function functionInsert(con,sql){
+var functionSelect = (queryString) => { // console.log(queryString);
+    return new Promise((resolve, reject) => {
+        con.query(queryString, function(err, rows, fields) {
+            if (err) {
+                resolve(false);
+                console.log(err);
+            } else {
+                console.log(rows);
+                resolve(rows);
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-  con.query(sql, function (err, result) {
-    if (err) throw err;
-    console.log("1 record inserted");
-  });
-con.end();
-});
+            }
+        })
+    })
 }
-
-/*
-function functionSelect(sql){
-
-con.connect(function(err) {
-  var x;
-  if (err) throw err;
-  console.log("Connected!");
-  con.query(sql, function (err, result) {
-    if (err) throw err;
-    console.log(JSON.parse(JSON.stringify(result)));
-    x=JSON.parse(JSON.stringify(result));
-    //return(JSON.parse(JSON.stringify(result)));
-    //return "helloworld";
-  });
-con.end();
-});
- return x;
-}
-*/
-
-
-
-var functionSelect = (queryString) => {     // console.log(queryString);     
-	return new Promise((resolve, reject) => {         
-		con.query(queryString, function(err, rows, fields) {             
-			if (err) {                 
-				resolve(false);                 
-				 console.log(err);             
-			}             
-			else {
-				console.log(rows);
-				resolve(rows); 
-				
-			}        
-		})     }) }
 
 
 module.exports = {
-        functionSelect: functionSelect
+    functionSelect: functionSelect
 }
-
 
--- backend/home_handler.js
+++ backend/home_handler.js
@@ -1,34 +1,31 @@
-function getGamesHandler(req,res){
-        var dbConnection = require('./db_connectio.js');
-        var zipcode=req.query.zipcode;
-        var sql=`SELECT id,game_type_id,name,DATE_FORMAT(DATE, '%Y-%m-%d') as date,players_count,cost,
+function getGamesHandler(req, res) {
+    var dbConnection = require('./db_connectio.js');
+    var zipcode = req.query.zipcode;
+    
+    var sql = `SELECT id,game_type_id,name,DATE_FORMAT(DATE, '%Y-%m-%d') as date,players_count,cost/100 as cost,
      json_object('apt',apt,'street',street,'city',city,'country',country,'zipcode',zipcode) AS
      address,created_by,DATE_FORMAT(created_on, '%Y-%m-%d') as created_on,owner_id,
-     IF(EXISTS(SELECT true FROM playalong.game_enrollment WHERE id = G.id)=1,'true','false') as hasJoined
-  FROM playalong.games G where zipcode=`+zipcode+";";
-  console.log(sql); 
-  /*
-   var sql=`SELECT id,game_type_id,name,DATE_FORMAT(DATE, '%Y-%m-%d') as date,players_count,cost,
-   CONCAT
-    ("{",
-      'apt:', '"', coalesce(apt,"")   , '"', ','
-      'street:', '"', street, '"', ','
-          'city:', '"', city, '"', ','
-           'country:', '"', country, '"', ','
-      'zipcode:'  , zipcode ,"}"
-    ) AS address,created_by,DATE_FORMAT(created_on, '%Y-%m-%d') as created_on,owner_id
-  FROM playalong.games where zipcode=`+zipcode+";";
-  console.log(sql);
-  */
-                (async()=>{
+     IF(EXISTS(SELECT true FROM playalong.game_enrollment WHERE id = G.id)=1,'True','False') as hasJoined,required_skill_level_id
 
-                        var result=await dbConnection.functionSelect(sql);
-		//	results = results.map(row => (row.package = JSON.stringify(row.package), row));
-                res.send(result);
-                })()
+  FROM playalong.games G where zipcode=` + zipcode + ";";
+    
+    
+//    var sql="call playalong.getHome("+zipcode+");";
+    console.log(sql);
+    (async() => {
+
+        var result = await dbConnection.functionSelect(sql);
+	//result = result.map(row => (row.package = JSON.stringify(row.package), row));
+        res.send(result);
+    })()
 }
 
+function completeEnroll(req, res) {
+res.send("hello")
+}
 module.exports = {
-        getGamesHandler:getGamesHandler
+    getGamesHandler: getGamesHandler,
+    completeEnroll:completeEnroll
+	
 }
 
