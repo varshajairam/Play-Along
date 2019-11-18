c2d105db19ae3070d22b4d0308f0f3450586e898
Kedar Acharya <kedaracharya@Kedars-MacBook-Pro.local>
2019-11-18 12:48:09 -0800

    routes for user register and game register -> for user register route is /register and game register route is /registergame

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,20 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
+const sql = require('mysql')
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
 
 var login_register_handler = require('./login_register_handler');
 
 app.post('/login', login_register_handler.loginHandler);
-app.post('/register', login_register_handler.registerHandler);
+app.post('/register', login_register_handler.registerUserHandler);
+app.post('/registergame', login_register_handler.registerGameHandler);
+
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- backend/login_register_handler.js
+++ backend/login_register_handler.js
@@ -1,12 +1,69 @@
-function loginHandler(req, res) {
-	res.send("Hello World");
+const mysql = require('mysql');
+
+const con = mysql.createConnection({
+  			host: "localhost",
+  			user: "root",
+  			password: "password",
+  			database : "PlayAlong"
+			});
+
+function q(con, sqlquery){
+	console.log(sqlquery)
+ 
+  	
 }
 
-function registerHandler(req, res) {
-	res.send("Hello World");
+  
+function queryHandler(sqlquery)
+{
+	//result = q(con,sqlquery);
+	return new Promise((resolve, reject) => {
+		con.query(sqlquery, function (err, result, fields) {
+		    if (err){
+		    	console.error(err);
+		    	reject(err);
+		    }
+			else if(result){
+				resolve(result);
+			}
+		});
+
+	});
+	
+}
+
+function loginHandler(req, res) {
+	res.send("Hello World");  
+}
+
+async function registerUserHandler(req, res) {
+		const query = "INSERT INTO user ( name, email, password, is_admin, dob, mobile,country, zipcode) VALUES ('"+req.body.name+"','"+req.body.email+"','"+req.body.password+"',"+req.body.is_admin+", '"+req.body.dob+"',"+req.body.mobile+", '"+req.body.country+"',"+req.body.zipcode+")";
+		await queryHandler(query);
+		
+		res.send("insert done");
+}
+
+async function registerGameHandler(req, res){
+	var x;
+	var game = (req.body.game).split(',');
+	var level = (req.body.level).split(',');
+	for (x=0 ; x<game.length; x++){
+	const query2 = "select id from games_enum where name= '"+game[x]+"'";
+	const query3 = "select id from skill_level_enum where level= '"+level[x]+"'";
+	const query5 = "select id from user where email='"+req.body.email+"'";
+	let result_game = await queryHandler(query2);
+	let result_skill = await queryHandler(query3);
+	let result_user = await queryHandler(query5);
+	const query4 = "INSERT INTO user_skills(user_id, game_id, skill_id) VALUES ("+result_user[0]['id']+", "+result_game[0]['id']+", "+result_skill[0]['id']+")";
+	await queryHandler(query4);
+		
+	}
+	res.send("done");
 }
 
 module.exports = {
 	loginHandler: loginHandler,
-	registerHandler: registerHandler
+	registerUserHandler: registerUserHandler,
+	registerGameHandler: registerGameHandler
+
 }
