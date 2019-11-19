1c76ea854d4b361fbb67b6731d549876b0e2d663
varsharp <my1daily1mail@gmail.com>
2019-11-18 16:59:40 -0800

    Merged with comm service

--- frontend/src/app/AppService/app.service.ts
+++ frontend/src/app/AppService/app.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Game } from '../AppClass/game';
+import { CommunicationService } from '../services/communication.service';
 
 @Injectable({
     providedIn: 'root'
@@ -10,13 +11,15 @@ import { Game } from '../AppClass/game';
 export class AppService {
     url: string;
     game: Game;
-    constructor(private http: HttpClient){
+    constructor(private http: HttpClient, private commService: CommunicationService){
 
     }
 
     getGames(userId: number): Observable<Array<Game>> {
-        this.url = "http://localhost:3000/getGames";
-        return this.http.get(this.url+"?id="+userId)
+        this.url = "getGames";
+        let data = {id : userId};
+        return this.commService.get(this.url, data)
+        //this.http.get(this.url+"?id="+userId)
         .pipe(map(res => {
             if(res instanceof Array){
                 return res.map(item =>{                    
