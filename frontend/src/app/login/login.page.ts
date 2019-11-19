da0dff0b433edf79c690939ded367ccc5a322bdb
Ronak Shah <ronakshah2895@gmail.com>
2019-11-18 16:15:20 -0800

    used communication service

--- frontend/src/app/login/login.page.ts
+++ frontend/src/app/login/login.page.ts
@@ -1,30 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-
+import { CommunicationService } from './../services/communication.service'
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
-  HttpUploadOptions = {
-    headers: new HttpHeaders({ "Content-Type": "application/x-www-form-urlencoded" })
-  }
+  
   formData = {username:"", password:""};
 
-  constructor(private http: HttpClient) { }
+  constructor(private comm: CommunicationService) { }
 
   ngOnInit() {
   }
 
-
-
   login(form){
-    let URLParams = new URLSearchParams();
-    URLParams.set("username", this.formData.username);
-    URLParams.set("password", this.formData.password);
-    this.http.post("http://localhost:3000/login", URLParams.toString(), this.HttpUploadOptions).subscribe(() => {
+    this.comm.sendPost("login", this.formData).subscribe(() => {
       console.log("Success");
     }, () => {
       console.log("Failed");
