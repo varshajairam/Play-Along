0d674f1a641a837cc83940c50639ceb40b3e14da
terrybelinda <terrylinda13@gmail.com>
2019-11-18 16:07:10 -0800

    renamed column required_skill_level_id

--- backend/home_handler.js
+++ backend/home_handler.js
@@ -5,7 +5,7 @@ function getGamesHandler(req, res) {
     var sql = `SELECT G.id,G.game_type_id,G.name,DATE_FORMAT(DATE, '%Y-%m-%d') as date,players_count,cost/100 as cost,
      json_object('apt',G.apt,'street',G.street,'city',G.city,'country',G.country,'zipcode',G.zipcode) AS
      address,created_by,DATE_FORMAT(created_on, '%Y-%m-%d') as created_on,owner_id,
-     IF(EXISTS(SELECT true FROM playalong.game_enrollment WHERE id = U.id)=1,'True','False') as hasJoined,required_skill_level_id
+     IF(EXISTS(SELECT true FROM playalong.game_enrollment WHERE id = U.id)=1,'True','False') as hasJoined,required_skill_level_id as required_skill
      FROM playalong.games G, playalong.user U where
      U.zipcode=G.zipcode and U.id=`+userId+";";
     
