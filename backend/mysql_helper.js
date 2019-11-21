949c8060cff36804ee074c0d0f9bf6a59c6d08a1
Ronak Shah <ronakshah2895@gmail.com>
2019-11-21 01:26:44 -0800

    added mysql helper and hashed password before storing

---
+++ backend/mysql_helper.js
@@ -0,0 +1,23 @@
+const mysql = require('mysql');
+const con = mysql.createConnection({
+	host: process.env.MYSQL_HOST,
+	user: process.env.MYSQL_USER,
+	password: process.env.MYSQL_PASSWORD,
+	database : process.env.MYSQL_DATABASE
+});
+
+function executeQuery(query, args) {
+	return new Promise((resolve, reject) => {
+		con.connect(function(err) {
+			if (err) reject(err);
+			con.query(query, args, function(err, result) {
+				if (err) reject(err);
+				resolve(result);
+			})
+		});
+	});
+}
+
+module.exports = {
+	executeQuery: executeQuery
+}
