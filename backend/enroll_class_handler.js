6f85f317e8ce8ffdfc4f86019aa97fd0c5206b03
terrylinda13@gmail.com <terrylinda13@gmail.com>
2019-11-24 16:34:26 -0800

    changes for cost

--- backend/enroll_class_handler.js
+++ backend/enroll_class_handler.js
@@ -7,10 +7,10 @@ function enrollClassHandler(req,res){
        var userID= req.user.id;
 	
 
-       console.log("cost:"+req.body.cost+" userID:"+userID+" instructor_id:"+req.body.instructor_id+" class_id:"+req.body.class_id);
+       console.log("cost:"+req.body.cost*100+" userID:"+userID+" instructor_id:"+req.body.instructor_id+" class_id:"+req.body.class_id);
        var sql= "call playalong.enrollClass(?,?,?,?,@result,@message);select @resultEnroll as resultEnroll,@enrollMessage as enrollResponse;";
 
-       mysql_helper.executeQuery(sql,[req.body.cost,userID,req.body.instructor_id,req.body.class_id]).then((result) => {
+       mysql_helper.executeQuery(sql,[req.body.cost*100,userID,req.body.instructor_id,req.body.class_id]).then((result) => {
                 
 	     	if(result[0].constructor.name=='OkPacket')
            		res.send(result[1]);
