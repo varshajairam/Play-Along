87cd5cd96fe7435615757f9099a7e3da79da9897
terrylinda13@gmail.com <terrylinda13@gmail.com>
2019-11-24 16:53:30 -0800

    changes for amount

--- backend/enroll_handler.js
+++ backend/enroll_handler.js
@@ -2,13 +2,13 @@ function enrollGamesHandler(req, res) {
 
     var mysql_helper = require('./mysql_helper');
     var userID=req.user.id;
-   // var userID=1;
+    //var userID=1;
     
 
     var query="call playalong.enroll(?,?,?,?,@result,@enroll);select @resultEnroll as resultEnroll,@enrollMessage as enrollResponse;"
 
-    console.log("Amount:"+req.body.amount+" user_id:"+userID+" owner_id:"+req.body.owner_id+" game_id:"+req.body.game_id);
-    mysql_helper.executeQuery(query,[req.body.amount, userID,req.body.owner_id, req.body.game_id]).then((result) => {
+    console.log("Amount:"+req.body.amount*100+" user_id:"+userID+" owner_id:"+req.body.owner_id+" game_id:"+req.body.game_id);
+    mysql_helper.executeQuery(query,[req.body.amount*100, userID,req.body.owner_id, req.body.game_id]).then((result) => {
 			
                 if(result[0].constructor.name=='OkPacket')
                         res.send(result[1]);
