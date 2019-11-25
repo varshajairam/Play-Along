64484705d6c396ce78798b9893faf75b8ef11363
terrylinda13@gmail.com <terrylinda13@gmail.com>
2019-11-24 21:48:54 -0800

    changes after testing home_handler

--- backend/home_handler.js
+++ backend/home_handler.js
@@ -3,11 +3,10 @@ function getGamesHandler(req, res) {
     //var userId = 1;
     var userId= req.user.id;
    
-    
+     
     console.log("UserID: " + userId+" zipcode:"+ req.query.zipcode) ;
     var sql="call playalong.getHome ?;";
-    const values = [[[userId,req.body.zipcode]]];
-
+    const values = [[[userId,req.query.zipcode]]];
     mysql_helper.executeQuery(sql, values).then((result) => {
 			res.send(result[0]);
 		});
