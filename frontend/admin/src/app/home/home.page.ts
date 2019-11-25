91dc6b9b2efcdcb80023637c5c45ecd08a567f46
Ronak Shah <ronakshah2895@gmail.com>
2019-11-24 16:26:31 -0800

    create front end for user management

--- frontend/admin/src/app/home/home.page.ts
+++ frontend/admin/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CommunicationService } from './../services/communication.service';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,26 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+	segmentValue = "activeUsers";
+	users: any = []
+
+  	constructor(private comm: CommunicationService) {
+  		this.fetch_users();
+  	}
+
+  	fetch_users() {
+  		this.comm.sendPost('getAllUsers').subscribe((result) => {
+  			this.users = result;
+  		});
+  	}
+
+  	update_user_status(index, user_email, is_blocked) {
+  		this.comm.sendPost('updateUserStatus', {
+  			is_blocked: is_blocked,
+  			email: user_email
+  		}).subscribe(() => {
+  			this.users[index].is_blocked = is_blocked;
+  		});
+  	}
 
 }
